480366ce835515b2cdfc592da98adc92eeb60850
Pedro Oliveira <letter.pedro@gmail.com>
2022-09-26 07:32:50 -0300

    adding new migration

--- FoodInLoco.Application/Data/ContextFactory.cs
+++ FoodInLoco.Application/Data/ContextFactory.cs
@@ -7,7 +7,7 @@ namespace FoodInLoco.Application.Data
     {
         public Context CreateDbContext(string[] args)
         {
-            const string connectionString = "Host=ec2-34-207-12-160.compute-1.amazonaws.com;Database=d341nsi2add60v;Username=dqfsvxtbmltnef;Password=2458f4cff34fa349a406a4dae4501e5020f24d293f079097c311d833de2e0e45";
+            const string connectionString = "Host=ec2-44-207-253-50.compute-1.amazonaws.com;Database=d5l5apig829nrt;Username=lkagcwvzznyyeh;Password=9858fa2603ac764902f174a7df75e241169f9c5f18fa2c3460f1ee29d3a8cf60";
 
             return new Context(new DbContextOptionsBuilder<Context>().UseNpgsql(connectionString).Options);
         }
